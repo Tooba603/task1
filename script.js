443c9d91a976fb786e642fc9d0fed5f1916c5ca9
Tooba603 <toobaakbar96@gmail.com>
2021-07-19 20:49:16 +0500

    Add multiple textareas with different height and width

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
 function changefontsize() {
-  var myInput = document.getElementById('myInput');
-  if(isOverflown(myInput)) {
-  
-    while (isOverflown(myInput)){
-    currentfontsize--;
-    myInput.style.fontSize = currentfontsize + 'px';
+
+  var myInputs = document.querySelectorAll("textarea");
+  for(var i = 0; i< myInputs.length; i++){
+    var myInput = myInputs[i];
+    if(isOverflown(myInput)) {
+      while (isOverflown(myInput)){
+        currentfontsize--;
+        myInput.style.fontSize = currentfontsize + 'px';
+      }
     }
-  }else {
-    currentfontsize = 13;
-    myInput.style.fontSize = currentfontsize + 'px';
-    while (isOverflown(myInput)){
-    currentfontsize--;
-    myInput.style.fontSize = currentfontsize + 'px';
+    else {
+      currentfontsize = 13;
+      myInput.style.fontSize = currentfontsize + 'px';
+      while (isOverflown(myInput)){
+      currentfontsize--;
+      myInput.style.fontSize = currentfontsize + 'px';
+      }
     }
-  }	
+  }
 }
 
 function isOverflown(element) {
-    return element.scrollHeight > element.clientHeight;
+  return element.scrollHeight > element.clientHeight;
 }
-
-
-
-
-
-
-
- 
