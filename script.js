76b7037c8c5755dedb0accd473c5639112fccb41
Tooba603 <toobaakbar96@gmail.com>
2021-07-27 10:30:00 +0500

    Fix Enter is pressed and font-size is not decreased and when paste characters font-size is auto adjust

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 function changefontsize() {
-
   var myInputs = document.querySelectorAll("textarea");
-  for(var i = 0; i< myInputs.length; i++){
+  for (var i = 0; i < myInputs.length; i++) {
     var myInput = myInputs[i];
-    if(isOverflown(myInput)) {
-      while (isOverflown(myInput)){
+    myInput.addEventListener("paste", changefontsize);
+
+    if (isOverflown(myInput)) {
+      while (isOverflown(myInput)) {
         currentfontsize--;
-        myInput.style.fontSize = currentfontsize + 'px';
+        myInput.style.fontSize = currentfontsize + "px";
+        //   myInput.addEventListener("keydown", (e) => {
+        //     if(e.key === 'Backspace'){
+        //       console.log(currentfontsize,"back is pressed");
+        //       if(currentfontsize < 13){
+        //         currentfontsize++;
+        //       myInput.style.fontSize = currentfontsize + 'px';
+        //       // while(isOverflown(myInput)){
+        //       // currentfontsize++;
+        //       // myInput.style.fontSize = currentfontsize + 'px';
+        //       // }
+        //       }
+
+        //   }
+
+        //   });
       }
-    }
-    else {
+
+      myInput.addEventListener("keydown", preventEnter);
+    } else {
       currentfontsize = 13;
-      myInput.style.fontSize = currentfontsize + 'px';
-      while (isOverflown(myInput)){
-      currentfontsize--;
-      myInput.style.fontSize = currentfontsize + 'px';
+      myInput.style.fontSize = currentfontsize + "px";
+      while (isOverflown(myInput)) {
+        currentfontsize--;
+        myInput.style.fontSize = currentfontsize + "px";
       }
     }
   }
@@ -22,4 +39,11 @@ function changefontsize() {
 
 function isOverflown(element) {
   return element.scrollHeight > element.clientHeight;
+}
+
+function preventEnter(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    console.log("Enter is pressed");
+  }
 }
